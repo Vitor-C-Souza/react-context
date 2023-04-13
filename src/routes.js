77f3c5e775a974983a54196b2b965cea1bef240d
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-13 12:44:12 -0300

    entrar com um usuario, selecionar produtos da feira separadamente e removendo produtos deselecionando e os exbindo no carrinho usando contexto apenas sem precisa enviar props

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,30 @@
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
-import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { UsuarioProvider } from "common/context/Usuario";
+import { CarrinhoProvider } from "common/context/Carrinho";
 
 function Router() {
-  const [nome, setNome] = useState('');
-  const [saldo, setSaldo] = useState(0);
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Login
-            nome={nome}
-            setNome={setNome}
-            saldo={saldo}
-            setSaldo={setSaldo}
-          />
-        </Route>
-        <Route path="/feira">
-          <Feira />
-        </Route>
-        <Route path="/carrinho">
-          <Carrinho />
-        </Route>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <CarrinhoProvider>
+            <Route path="/feira">
+              <Feira />
+            </Route>
+            <Route path="/carrinho">
+              <Carrinho />
+            </Route>
+          </CarrinhoProvider>
+        </UsuarioProvider>
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default Router;
